b4678ed6c18cdb7ba4338a2a333daf54bcd4510c
Charlie Burrows <charlie.burrows@gmail.com>
2019-06-14 00:37:49 -0400

    Trying to voxelize

--- src/dla3d/core.clj
+++ src/dla3d/core.clj
@@ -2,7 +2,7 @@
   (:require
     [thi.ng.geom.core :as g]
     [thi.ng.geom.vector :refer [vec3]]
-    [thi.ng.geom.spatialtree :as st]
+    [kdtree :as kd]
     [thi.ng.geom.voxel.svo :as svo]
     [thi.ng.geom.voxel.isosurface :as iso]
     [thi.ng.geom.mesh.io :as mio]
@@ -10,39 +10,50 @@
     ))
 
 (defn new-rand-point [xyz-range]
-  (vec3 (- (rand xyz-range) (/ xyz-range 2))
-        (- (rand xyz-range) (/ xyz-range 2))
-        (- (rand xyz-range) (/ xyz-range 2))))
-(defn dla-points
-  "I don't do a whole lot."
-  [npoints start-range]
-  (let [tree (st/octree 0 0 0 100 100 100)]
-    (g/add-point tree (vec3 0 0 0) (vec3 0 0 0))
-    (loop [tree tree
-           i npoints
-           p (new-rand-point start-range)]
-      (println i p)
-      (cond (zero? i)
-            (st/select-with-sphere tree (vec3 0 0 0) 50)
+  [(- (rand (* 2 xyz-range)) xyz-range)
+   (- (rand (* 2 xyz-range)) xyz-range)
+   (- (rand (* 2 xyz-range)) xyz-range)])
 
-            (st/points-in-sphere? tree p 5) 
-            (recur (g/add-point tree p p)
-                   (dec i)
-                   (new-rand-point start-range))
+;;;;;;;;;; KD-TREE Particle Approach ;;;;;;;;;;;;;;
+(defn stick-particle [tree]
+  (let [sz 25
+        radius 1.0
+        touch-dist-sq (* 2 radius 2 radius)] 
+    (loop [p (new-rand-point sz)]
+      (let [near (kd/nearest-neighbor tree p 2)]
+        (cond
+          (and (= 2 (count near))
+               (< (:dist-squared (first near)) touch-dist-sq)
+               (< (:dist-squared (second near)) touch-dist-sq))
+          (recur (new-rand-point sz))
 
-            (not (st/points-in-sphere? tree p 10))
-            (recur tree i (new-rand-point start-range))
+          (< (:dist-squared (first near)) touch-dist-sq)
+          (vec p)
+          ;[p (first near)]
 
-            :otherwise 
-            (recur tree i (.+ p (new-rand-point 1.)))))))
+          (> (:dist-squared (first near)) (* 5 touch-dist-sq))
+          (recur (new-rand-point sz))
+
+          :otherwise
+          (recur (map + p (new-rand-point 2))))))))
+
+(defn aggregate [nparts]
+  (second
+    (reduce (fn [[tree ps] _] 
+              (let [p (stick-particle tree)]
+                [(kd/insert tree p) (conj ps p)]))
+            [(kd/build-tree [[0 0 0]]) [[0 0 0]]]
+            (range nparts))))
 
 
-;(defn voxelize-points [points]
-;  (let [scale (apply max (mapcat vec points))])
-;  (svo/apply-voxels svo/set-at (svo/voxeltree 32 res) points))
-;
-;(defn main []
-;  (with-open [o (io/output-stream "dla.stl")]
-;   (mio/write-stl
-;    (mio/wrapped-output-stream o)
-;    (g/tessellate (iso/surface-mesh v 10 iso-val)))))
+(def res 0.25)
+(defn voxelize-points [points]
+  (let [scale (apply max (mapcat vec points))]
+    (svo/apply-voxels svo/set-at (svo/voxeltree 32 res) points)))
+
+(def iso-val 0.5)
+(defn main [points]
+  (with-open [o (io/output-stream "dla.stl")]
+   (mio/write-stl
+    (mio/wrapped-output-stream o)
+    (g/tessellate (iso/surface-mesh (voxelize-points points) 10 iso-val)))))
